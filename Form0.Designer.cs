b8efd2693a882a509d8ee0c0e889ae33022ffd1e
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-04-29 19:27:02 +0800

    Update Form0.Designer.cs

--- Form0.Designer.cs
+++ Form0.Designer.cs
@@ -28,186 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.text_name = new System.Windows.Forms.TextBox();
-            this.text_id = new System.Windows.Forms.TextBox();
-            this.text_user = new System.Windows.Forms.TextBox();
-            this.text_class = new System.Windows.Forms.TextBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.save = new System.Windows.Forms.Button();
-            this.text_password = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 39);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 21);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Student id";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 78);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(99, 21);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Username";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 117);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(95, 21);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Password";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(3, 156);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 21);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Class";
-            // 
-            // text_name
-            // 
-            this.text_name.Location = new System.Drawing.Point(151, 3);
-            this.text_name.Name = "text_name";
-            this.text_name.Size = new System.Drawing.Size(142, 33);
-            this.text_name.TabIndex = 5;
-            // 
-            // text_id
-            // 
-            this.text_id.Location = new System.Drawing.Point(151, 42);
-            this.text_id.Name = "text_id";
-            this.text_id.Size = new System.Drawing.Size(142, 33);
-            this.text_id.TabIndex = 6;
-            // 
-            // text_user
-            // 
-            this.text_user.Location = new System.Drawing.Point(151, 81);
-            this.text_user.Name = "text_user";
-            this.text_user.Size = new System.Drawing.Size(142, 33);
-            this.text_user.TabIndex = 7;
-            // 
-            // text_class
-            // 
-            this.text_class.Location = new System.Drawing.Point(151, 159);
-            this.text_class.Name = "text_class";
-            this.text_class.Size = new System.Drawing.Size(142, 33);
-            this.text_class.TabIndex = 9;
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.tableLayoutPanel1.AutoSize = true;
-            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.save, 1, 5);
-            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
-            this.tableLayoutPanel1.Controls.Add(this.text_password, 1, 3);
-            this.tableLayoutPanel1.Controls.Add(this.text_class, 1, 4);
-            this.tableLayoutPanel1.Controls.Add(this.text_name, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.text_user, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 5);
-            this.tableLayoutPanel1.Controls.Add(this.text_id, 1, 1);
-            this.tableLayoutPanel1.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(286, 121);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(296, 232);
-            this.tableLayoutPanel1.TabIndex = 10;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(130, 21);
-            this.label1.TabIndex = 12;
-            this.label1.Text = "Student name";
-            // 
-            // save
-            // 
-            this.save.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.save.AutoSize = true;
-            this.save.Location = new System.Drawing.Point(151, 198);
-            this.save.Name = "save";
-            this.save.Size = new System.Drawing.Size(142, 31);
-            this.save.TabIndex = 11;
-            this.save.Text = "Save";
-            this.save.UseVisualStyleBackColor = true;
-            this.save.Click += new System.EventHandler(this.save_Click);
-            // 
-            // text_password
-            // 
-            this.text_password.Location = new System.Drawing.Point(151, 120);
-            this.text_password.Name = "text_password";
-            this.text_password.Size = new System.Drawing.Size(142, 33);
-            this.text_password.TabIndex = 11;
-            // 
-            // button2
-            // 
-            this.button2.AutoSize = true;
-            this.button2.Location = new System.Drawing.Point(3, 198);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(142, 31);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Close";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // label6
-            // 
-            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("PMingLiU", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label6.Location = new System.Drawing.Point(26, 18);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(764, 64);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Create New Student Account";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.label6.Click += new System.EventHandler(this.label6_Click);
-            // 
-            // Form0
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(804, 448);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "Form0";
-            this.Text = "Form0";
-            this.Load += new System.EventHandler(this.Form0_Load);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.text_name = new System.Windows.Forms.TextBox();
+			this.text_id = new System.Windows.Forms.TextBox();
+			this.text_user = new System.Windows.Forms.TextBox();
+			this.text_class = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.save = new System.Windows.Forms.Button();
+			this.text_password = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 39);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 21);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Student id";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(3, 78);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(99, 21);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Username";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(3, 117);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(95, 21);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Password";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(3, 156);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(58, 21);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "Class";
+			// 
+			// text_name
+			// 
+			this.text_name.Location = new System.Drawing.Point(151, 3);
+			this.text_name.Name = "text_name";
+			this.text_name.Size = new System.Drawing.Size(142, 33);
+			this.text_name.TabIndex = 5;
+			// 
+			// text_id
+			// 
+			this.text_id.Location = new System.Drawing.Point(151, 42);
+			this.text_id.Name = "text_id";
+			this.text_id.Size = new System.Drawing.Size(142, 33);
+			this.text_id.TabIndex = 6;
+			// 
+			// text_user
+			// 
+			this.text_user.Location = new System.Drawing.Point(151, 81);
+			this.text_user.Name = "text_user";
+			this.text_user.Size = new System.Drawing.Size(142, 33);
+			this.text_user.TabIndex = 7;
+			// 
+			// text_class
+			// 
+			this.text_class.Location = new System.Drawing.Point(151, 159);
+			this.text_class.Name = "text_class";
+			this.text_class.Size = new System.Drawing.Size(142, 33);
+			this.text_class.TabIndex = 9;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.tableLayoutPanel1.AutoSize = true;
+			this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.save, 1, 5);
+			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+			this.tableLayoutPanel1.Controls.Add(this.label5, 0, 4);
+			this.tableLayoutPanel1.Controls.Add(this.text_password, 1, 3);
+			this.tableLayoutPanel1.Controls.Add(this.text_class, 1, 4);
+			this.tableLayoutPanel1.Controls.Add(this.text_name, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.text_user, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.button2, 0, 5);
+			this.tableLayoutPanel1.Controls.Add(this.text_id, 1, 1);
+			this.tableLayoutPanel1.Font = new System.Drawing.Font("PMingLiU", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(286, 121);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 6;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(296, 232);
+			this.tableLayoutPanel1.TabIndex = 10;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(130, 21);
+			this.label1.TabIndex = 12;
+			this.label1.Text = "Student name";
+			// 
+			// save
+			// 
+			this.save.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.save.AutoSize = true;
+			this.save.Location = new System.Drawing.Point(151, 198);
+			this.save.Name = "save";
+			this.save.Size = new System.Drawing.Size(142, 31);
+			this.save.TabIndex = 11;
+			this.save.Text = "Save";
+			this.save.UseVisualStyleBackColor = true;
+			this.save.Click += new System.EventHandler(this.save_Click);
+			// 
+			// text_password
+			// 
+			this.text_password.Location = new System.Drawing.Point(151, 120);
+			this.text_password.Name = "text_password";
+			this.text_password.Size = new System.Drawing.Size(142, 33);
+			this.text_password.TabIndex = 11;
+			// 
+			// button2
+			// 
+			this.button2.AutoSize = true;
+			this.button2.Location = new System.Drawing.Point(3, 198);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(142, 31);
+			this.button2.TabIndex = 13;
+			this.button2.Text = "Close";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label6
+			// 
+			this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("PMingLiU", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label6.Location = new System.Drawing.Point(26, 18);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(764, 64);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Create New Student Account";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.label6.Click += new System.EventHandler(this.label6_Click);
+			// 
+			// Form0
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(804, 448);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "Form0";
+			this.Text = "Form0";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form0_FormClosing);
+			this.Load += new System.EventHandler(this.Form0_Load);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
